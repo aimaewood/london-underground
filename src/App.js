b5e385a22c72ba5147a1df8372890a6e9327ba61
aimaewood <aimaewood@gmail.com>
2022-02-08 05:47:22 +0000

    isolating data for selected two train stations

--- src/App.js
+++ src/App.js
@@ -1,23 +1,62 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from "react";
+import {useEffect} from "react";
+import {Dropdown} from "./components/Dropdown";
+
 
 function App() {
+    let[stationFrom, updateFrom] = useState("")
+    let[stationTo, updateTo] = useState("")
+    let [allStationData, setStations] = useState([])
+    let directLines = []
+    useEffect(() => {
+            fetch('/london-underground-stations.json')
+                .then(data => data.json())
+                .then(results => {
+                    setStations(results.stations)
+                })
+        }
+    )
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div className="container">
+            <div className="intro">
+                Plan your journey...
+            </div>
+            <form>
+                <Dropdown directionLabel={"From:"}
+                          direction={"from"}
+                          changeHandler={(event) => updateFrom(event.target.value)}/>
+                <Dropdown directionLabel={"To:"}
+                          direction={"to"}
+                          changeHandler={(event) => updateTo(event.target.value)}/>
+                <input className="input" type="submit" value="Find route"
+                       onClick={(event) => {
+                           let station1 = allStationData.filter(station => station.name.includes
+                           (stationFrom))
+                           let station2 = allStationData.filter(station => station.name.includes
+                           (stationTo))
+
+
+                           /*lines2.push(allStationData.filter(station => station.name.includes
+                           (stationTo)).map(station1 => station1.lines))
+                           for(let i=0; i<lines1.length; i++){
+                               for(let n=0; n<lines2.length; n++){
+                                   if(lines1[i] === lines2[n]){
+                                       directLines.push(lines1[i])
+                                       break
+                                   }
+                               }
+                           }*/
+                           event.preventDefault()
+                           console.log(allStationData)
+                           console.log(station2.lines)
+                       }
+                }/>
+
+            </form>
+        </div>
     </div>
   );
 }
